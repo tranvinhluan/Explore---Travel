1912ba606f78f0c245a9d9b990c8d616868012c2
tranvinhluan <tranvinhluanluuxa@gmail.com>
2022-01-21 11:50:29 +0700

    first up

---
+++ .history/app_20220121112714.js
@@ -0,0 +1,91 @@
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+const activeClass = "is-show";
+
+toggle.addEventListener("click", function () {
+  menu.classList.add(activeClass);
+});
+
+window.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+    menu.classList.remove(activeClass);
+  }
+});
+
+// window.addEventListener("load", function () {
+//   debugger
+//     const sliderItems = document.querySelectorAll(".testimonial-item");
+//     const sliderItemWidth = sliderItems[0].clientWidth;
+//     // console.log("sliderItemWidth", sliderItemWidth);
+//     const slider = document.querySelector(".testimonial-list");
+//     const arrowLeft = document.querySelector(".arrow-left");
+//     const arrowRight = document.querySelector(".arrow-right");
+//     let positionX = 0;
+//     let index = 0;
+
+//     arrowRight.addEventListener("click", function () {
+//       arrowLeft.classList.remove("disabled");
+//       if (index >= sliderItems.length - 1) {
+//         this.classList.add("disabled");
+//         return;
+//       }
+//       positionX += sliderItemWidth;
+//       index++;
+//       slider.setAttribute("style", `transform:translateX(-${positionX}px);`);
+//     });
+
+//     arrowLeft.addEventListener("click", function () {
+//       arrowRight.classList.remove("disabled");
+//       if (index <= 0) {
+//         this.classList.add("disabled");
+//         return;
+//       }
+//       positionX -= sliderItemWidth;
+//       index--;
+//       slider.setAttribute("style", `transform:translateX(-${positionX}px);`);
+//     });
+// });
+
+
+function slider () {
+  const sliderItems = document.querySelectorAll(".testimonial-item");
+    const sliderItemWidth = sliderItems[0].clientWidth;
+    const slider = document.querySelector(".testimonial-list");
+    const arrowLeft = document.querySelector(".arrow-left");
+    const arrowRight = document.querySelector(".arrow-right");
+    let positionX = 0;
+    let index = 0;
+
+    arrowRight.addEventListener("click", function () {
+      arrowLeft.classList.remove("disabled");
+      if (index >= sliderItems.length - 1) {
+        this.classList.add("disabled");
+        return;
+      }
+      positionX += sliderItemWidth;
+      index++;
+      slider.setAttribute("style", `transform:translateX(-${positionX}px);`);
+    });
+
+    arrowLeft.addEventListener("click", function () {
+      arrowRight.classList.remove("disabled");
+      if (index <= 0) {
+        this.classList.add("disabled");
+        return;
+      }
+      positionX -= sliderItemWidth;
+      index--;
+      slider.setAttribute("style", `transform:translateX(-${positionX}px);`);
+    });
+}
+
+
+
+window.addEventListener("load", function () {
+  this.a1()
+});
+
+window.addEventListener("resize", function () {
+  this.a1()
+});
+
